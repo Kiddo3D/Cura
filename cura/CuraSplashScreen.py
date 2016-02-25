b13853fbf70217f36819813a7ef9230b2653b637
Gabriel Constenla <grconstenla@gmail.com>
2016-02-25 19:44:55 -0300

    Splash screen

--- cura/CuraSplashScreen.py
+++ cura/CuraSplashScreen.py
@@ -12,21 +12,21 @@ class CuraSplashScreen(QSplashScreen):
     def __init__(self):
         super().__init__()
         self._scale = round(QFontMetrics(QCoreApplication.instance().font()).ascent() / 12)
-
+        
         splash_image = QPixmap(Resources.getPath(Resources.Images, "cura.png"))
         self.setPixmap(splash_image.scaled(splash_image.size() * self._scale))
 
     def drawContents(self, painter):
         painter.save()
-        painter.setPen(QColor(0, 0, 0, 255))
+        painter.setPen(Qt.white)
 
         version = Application.getInstance().getVersion().split("-")
 
-        painter.setFont(QFont("Proxima Nova Rg", 20 ))
-        painter.drawText(0, 0, 330 * self._scale, 230 * self._scale, Qt.AlignHCenter | Qt.AlignBottom, version[0])
+        painter.setFont(QFont("Proxima Nova Rg", 12 * self._scale))
+        painter.drawText(27 * self._scale, 0, 276 * self._scale, 250 * self._scale, Qt.AlignRight | Qt.AlignBottom, "Cura " + version[0])
         if len(version) > 1:
-            painter.setFont(QFont("Proxima Nova Rg", 12 ))
-            painter.drawText(0, 0, 330 * self._scale, 255 * self._scale, Qt.AlignHCenter | Qt.AlignBottom, version[1])
+            painter.setFont(QFont("Proxima Nova Rg", 8 * self._scale))
+            painter.drawText(27 * self._scale, 0, 276 * self._scale, 265 * self._scale, Qt.AlignRight | Qt.AlignBottom, version[1])
 
         painter.restore()
         super().drawContents(painter)
